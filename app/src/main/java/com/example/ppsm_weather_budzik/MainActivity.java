bf8e0249bb7a1e1ce01da30c6d953614a5d0684a
Marshall <kamil.budzik98@gmail.com>
2020-05-04 19:03:21 +0200

    dorobione polecenia z lab8

--- app/src/main/java/com/example/ppsm_weather_budzik/MainActivity.java
+++ app/src/main/java/com/example/ppsm_weather_budzik/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.ppsm_weather_budzik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -40,39 +43,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getWeatherData(View view) throws UnsupportedEncodingException {
+        if (checkNetworkConnection()) {
+            cityName = cityNameInput.getText().toString();
+            intent = new Intent(this, WeatherActivity.class);
+            intent.putExtra("CITY_NAME", cityName);
+            System.out.println(cityName);
+            Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.openweathermap.org/").
+                    addConverterFactory(GsonConverterFactory.create()).build();
 
-        cityName = cityNameInput.getText().toString();
-        intent = new Intent(this, WeatherActivity.class);
-        intent.putExtra("CITY_NAME", cityName);
-
-        System.out.println(cityName);
-
-        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.openweathermap.org/").
-                addConverterFactory(GsonConverterFactory.create()).build();
-
-        JsonPlaceholderAPI jsonPlaceholderAPI = retrofit.create(JsonPlaceholderAPI.class);
-
-        Call<WeatherData> call = jsonPlaceholderAPI.getMainWeatherClassCall(cityName + ",pl", apiid, units);
-        call.enqueue(new Callback<WeatherData>() {
-            @Override
-            public void onResponse(Call<WeatherData> call, Response<WeatherData> response) {
-                if (!response.isSuccessful()) {
-                    System.out.println("Code: " + response.code());
-                    error.setText("Błąd!");
-                    return;
+            JsonPlaceholderAPI jsonPlaceholderAPI = retrofit.create(JsonPlaceholderAPI.class);
+            Call<WeatherData> call = jsonPlaceholderAPI.getMainWeatherClassCall(cityName + ",pl", apiid, units);
+            call.enqueue(new Callback<WeatherData>() {
+                @Override
+                public void onResponse(Call<WeatherData> call, Response<WeatherData> response) {
+                    if (!response.isSuccessful()) {
+                        System.out.println("Code: " + response.code());
+                        error.setText("Błąd!");
+                        return;
+                    }
+                    weather = response.body();
+                    intent.putExtra("WEATHER_CLASS", weather);
+                    startActivity(intent);
+                    saveUsersLastCity(cityName);
+                    error.setText("");
                 }
-                weather = response.body();
-                intent.putExtra("WEATHER_CLASS", weather);
-                startActivity(intent);
-                saveUsersLastCity(cityName);
-                error.setText("");
-            }
 
-            @Override
-            public void onFailure(Call<WeatherData> call, Throwable t) {
-                System.out.println(t.getMessage());
-            }
-        });
+                @Override
+                public void onFailure(Call<WeatherData> call, Throwable t) {
+                    System.out.println(t.getMessage());
+                }
+            });
+        } else {
+            error.setText("Brak połączenia z internetem!");
+        }
     }
 
     private void saveUsersLastCity(String name) {
@@ -86,6 +89,30 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
         cityName = sharedPreferences.getString("CITY_KEY", "");
         cityNameInput.setText(cityName);
+    }
+
+    public static Call<WeatherData> findData(String city) {
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.openweathermap.org/").
+                addConverterFactory(GsonConverterFactory.create()).build();
+
+        JsonPlaceholderAPI jsonPlaceholderAPI = retrofit.create(JsonPlaceholderAPI.class);
+        Call<WeatherData> call = jsonPlaceholderAPI.getMainWeatherClassCall(city + ",pl", "749561a315b14523a8f5f1ef95e45864", "metric");
+        return call;
+    }
+
+    public boolean checkNetworkConnection() {
+        boolean wifi = false;
+        boolean mobile = false;
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mMobile = connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if (mWifi.isConnected()) {
+            wifi = true;
+        }
+        if (mMobile.isConnected()) {
+            mobile = true;
+        }
+        return wifi || mobile;
     }
 }
 
--- app/src/main/java/com/example/ppsm_weather_budzik/WeatherActivity.java
+++ app/src/main/java/com/example/ppsm_weather_budzik/WeatherActivity.java
@@ -1,11 +1,17 @@
 package com.example.ppsm_weather_budzik;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ppsm_weather_budzik.classes.WeatherData;
 import com.squareup.picasso.Picasso;
@@ -13,13 +19,20 @@ import com.squareup.picasso.Picasso;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class WeatherActivity extends AppCompatActivity {
 
     private String iconUrl = "https://openweathermap.org/img/wn/";
     private String location;
     private TextView cityName, time, temperatureValue, pressureValue, humidityValue, windSpeed;
-    private WeatherData weather;
+    private WeatherData weatherData;
     private ImageView icon;
+    SwipeRefreshLayout swipe;
+    Toast errorToast;
 
 
     @Override
@@ -27,11 +40,10 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
-
         Intent intent = getIntent();
         location = intent.getStringExtra("CITY_NAME");
-        weather = (WeatherData) intent.getSerializableExtra("WEATHER_CLASS");
-        iconUrl = iconUrl + weather.getWeather()[0].getIcon() + "@2x.png";
+        weatherData = (WeatherData) intent.getSerializableExtra("WEATHER_CLASS");
+        iconUrl = iconUrl + weatherData.getWeather()[0].getIcon() + "@2x.png";
         System.out.println(iconUrl);
 
         cityName = findViewById(R.id.cityName);
@@ -40,16 +52,95 @@ public class WeatherActivity extends AppCompatActivity {
         pressureValue = findViewById(R.id.pressureValue);
         humidityValue = findViewById(R.id.humidityValue);
         icon = findViewById(R.id.weatherIcon);
-        time.setText(LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
         windSpeed = findViewById(R.id.windSpeedLevel);
+        swipe = findViewById(R.id.swipeRefresh);
+        updateWeatherData(weatherData);
 
+        swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                if (checkNetworkConnection()) {
+                    refreshData();
+                    swipe.setRefreshing(false);
+                    errorToast = Toast.makeText(getApplicationContext(), "Pobrano nowe informacje o pogodzie.", Toast.LENGTH_SHORT);
+                    errorToast.setGravity(Gravity.TOP, 0, 200);
+                    errorToast.show();
+                } else {
+                    errorToast = Toast.makeText(getApplicationContext(), "Brak połączenia z internetem!", Toast.LENGTH_SHORT);
+                    errorToast.setGravity(Gravity.TOP, 0, 200);
+                    errorToast.show();
+                    swipe.setRefreshing(false);
+                }
+            }
+        });
+
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(5000);
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                if (checkNetworkConnection()) {
+                                    refreshData();
+                                    errorToast = Toast.makeText(getApplicationContext(), "Automatycznie pobrano nowe informacje o pogodzie.", Toast.LENGTH_SHORT);
+                                    errorToast.setGravity(Gravity.TOP, 0, 200);
+                                    errorToast.show();
+                                } else {
+                                    errorToast = Toast.makeText(getApplicationContext(), "Brak połączenia z internetem!", Toast.LENGTH_SHORT);
+                                    errorToast.setGravity(Gravity.TOP, 0, 200);
+                                    errorToast.show();
+                                }
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                    System.out.println("ERROR");
+                }
+            }
+        };
+        thread.start();
+    }
+
+    private void refreshData() {
+        Call<WeatherData> call = MainActivity.findData((String) cityName.getText());
+        call.enqueue(new Callback<WeatherData>() {
+            @Override
+            public void onResponse(Call<WeatherData> call, Response<WeatherData> response) {
+                weatherData = response.body();
+                updateWeatherData(weatherData);
+            }
+
+            @Override
+            public void onFailure(Call<WeatherData> call, Throwable t) {
+                System.out.println("error");
+            }
+        });
+    }
+
+    public void updateWeatherData(WeatherData weather) {
         Picasso.get().load(iconUrl).error(R.drawable.error).into(icon);
         cityName.setText(location);
         temperatureValue.setText(Double.toString(weather.getMain().getTemp()) + " °C");
         pressureValue.setText(Integer.toString(weather.getMain().getPressure()) + " hPa");
         humidityValue.setText(Integer.toString(weather.getMain().getHumidity()) + " %");
         windSpeed.setText(Double.toString(weather.getWind().getSpeed()) + " km/h");
+        time.setText(LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+
     }
 
+    public boolean checkNetworkConnection() {
+        boolean wifi = false;
+        boolean mobile = false;
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mMobile = connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if (mWifi.isConnected()) {
+            wifi = true;
+        }
+        if (mMobile.isConnected()) {
+            mobile = true;
+        }
+        return wifi || mobile;
+    }
 }
-
